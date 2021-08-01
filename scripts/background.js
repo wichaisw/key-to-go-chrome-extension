6aa4d14cae3044d07560c1c8f6e3d9586c7f0d99
wichaisw <wichai.sw19@gmail.com>
2021-08-01 18:04:37 +0700

    ADD roughly finish main feature

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,64 @@
-chrome?.tabs?.onActivated.addListener(tab => {
-  console.log("tab: ")
-  console.log(tab)
-  chrome.tabs.get(tab.tabId, current_tab_info => {
-    console.log(current_tab_info.url)
-  });
-  chrome.tabs.executeScript(
-    null, 
-    { file: './scripts/keyListener.js' },
-    () => { console.log("injected")}
-);
-});
+// let konamiIndex = 0;
+// let googleIndex = 0;
+// let hadokenIndex = 0;
+// let reactIndex = 0;
+// window.onkeydown = function(event) {
 
+//   console.log(event.keyCode)
+  
+//   let konamiCode = [38,38,40,40,37,39,37,39,66,65];
+//   let googleCode = [17, 71];
+//   let hadokenCode = [40,39,90];
+//   let reactCode = [17,74, 83, 88];
+  
+//   console.log("reactCode.length", reactCode.length)
+//   console.log(reactIndex)
+//   // event.keyCode === konamiCode[konamiIndex] ? konamiIndex++ : konamiIndex = 0;
+//   // event.keyCode === googleCode[googleIndex] ? googleIndex++ : googleIndex = 0;
+//   // event.keyCode === hadokenCode[hadokenIndex] ? hadokenIndex++ : hadokenIndex = 0;
+//   // event.keyCode === reactCode[reactIndex] ? reactIndex++ : reactIndex = 0;
+
+//   switch(event.keyCode) {
+//     case reactCode[reactIndex]:
+//       reactIndex++
+//       break;
+//     // case konamiCode[konamiIndex]:
+//     //   konamiIndex++
+//     //   break;
+//     // case googleCode[googleIndex]:
+//     //   googleIndex++
+//     //   break;
+//     // case hadokenCode[hadokenIndex]:
+//     //   hadokenIndex++
+//     //   break;
+//     default: 
+//       konamiIndex = 0;
+//       hadokenIndex = 0;
+//       googleIndex = 0;
+//       reactIndex = 0;
+//       break;
+//   }
+
+
+//   if(reactCode.length === reactIndex) {
+//     window.open('http://localhost:3000');
+//     reactIndex = 0;
+//   }
+
+//   // if(konamiCode.length === konamiIndex) {
+//   //   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+//   //   konamiIndex = 0;
+//   // }
+
+//   // if(googleCode.length === googleIndex) {
+//   //   window.open('https://www.google.co.th');
+//   //   googleIndex = 0;
+//   // }
+
+//   // if(hadokenCode.length === hadokenIndex) {
+//   //   window.open('https://streetfighter.com/');
+//   //   hadokenCode = 0;
+//   // }
+
+
+// }
--- scripts/keyListener.js
+++ scripts/keyListener.js
@@ -1,34 +1,64 @@
 // const constants = require('/constant.js');
 
 document?.addEventListener('DOMContentLoaded', function() {
-  let konamiCode = [
-    38,38,40,40,37,39,37,39,66,65
-  ]
+  let konamiIndex = 0;
+  let googleIndex = 0;
+  let hadokenIndex = 0;
+  let reactIndex = 0;
 
-  let index = 0;
+  let konamiCode = [38,38,40,40,37,39,37,39,66,65];
+  let googleCode = [17, 71];
+  let hadokenCode = [40,39,90];
+  let reactCode = [17,74, 83, 88];
   
   document?.addEventListener('keydown', onKeyDown, false);
-  // document?.querySelector('#alert-btn').addEventListener('click', onClick, false);
-
-  function onClick() {
-    console.log("tests")
-    alert("test")
-    chrome?.tabs.query({
-      currentWindow: true,
-      active: true
-    }, function (tabs) {
-        chrome?.tabs.sendMessage(tabs[0].id, 'hi');
-        console.log("tabs:", tabs)
-      }
-    );
-  }
 
   function onKeyDown(event) {
-    // console.warn(JSON.stringify())
-    event.keyCode === konamiCode[index] ? index++ : index = 0;
+    console.log("test")
 
-    if(konamiCode.length === index) {
-      alert("success")
+    // event.keyCode === konamiCode[konamiIndex] ? konamiIndex++ : konamiIndex = 0;
+    // event.keyCode === googleCode[googleIndex] ? googleIndex++ : googleIndex = 0;
+    // event.keyCode === hadokenCode[hadokenIndex] ? hadokenIndex++ : hadokenIndex = 0;
+    // event.keyCode === reactCode[reactIndex] ? reactIndex++ : reactIndex = 0;
+
+    switch(event.keyCode) {
+    case reactCode[reactIndex]:
+      reactIndex++
+      break;
+    case konamiCode[konamiIndex]:
+      konamiIndex++
+      break;
+    case googleCode[googleIndex]:
+      googleIndex++
+      break;
+    case hadokenCode[hadokenIndex]:
+      hadokenIndex++
+      break;
+    default: 
+      konamiIndex = 0;
+      hadokenIndex = 0;
+      googleIndex = 0;
+      reactIndex = 0;
+      break;
+  }
+
+    if(konamiCode.length === konamiIndex) {
+      window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
+
+    if(googleCode.length === googleIndex) {
+      window.open('https://www.google.co.th');
+    }
+
+    if(googleCode.length === googleIndex) {
+      window.open('https://www.google.co.th');
+      googleIndex = 0;
+    }
+  
+    if(hadokenCode.length === hadokenIndex) {
+      window.open('https://streetfighter.com/');
+      hadokenCode = 0;
     }
   }
 }, false);
+
